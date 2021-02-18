fa76a0942301541105c6007569d1c1af275f27e6
LLST-Learning-Games <mstevens@llst.ca>
2021-02-17 21:59:58 -0800

    0.0.1 2021 02 17 - First upload RTS Multiplayer course

---
+++ Assets/Scripts/Building/UnitSpawner.cs
@@ -0,0 +1,44 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject unitPrefab = null;
+    [SerializeField] private Transform spawnPoint = null;
+
+    
+
+    #region Server
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        GameObject unitInstance = Instantiate(
+            unitPrefab, 
+            spawnPoint.position, 
+            spawnPoint.rotation);
+
+        //Necessary to spawn the object on the server, to push it to all other clients
+        NetworkServer.Spawn(unitInstance, connectionToClient);  //connectionToClient from NetworkBehaviour, assigns ownership to the client
+    }
+
+    #endregion
+
+
+
+    #region Client
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button != PointerEventData.InputButton.Left) { return; }
+        if(!hasAuthority) { return; }   //only spawn the object if you've clicked on your own spawner (ie, not someone else's)
+
+        CmdSpawnUnit();
+        
+    }
+
+    #endregion
+}
